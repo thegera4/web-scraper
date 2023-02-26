385819af3fd19f45bec7d242ee04918163ac6079
thegera4 <84020433+thegera4@users.noreply.github.com>
2023-02-26 14:38:43 -0600

    update: neetlify deploy

---
+++ functions/index.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
+import express from 'express';
+import ServerlessHttp from 'serverless-http';
+
+export const app = express();
+const url = 'https://www.elsiglodetorreon.com.mx';
+
+app.get('/', (req, res) => {
+  axios(url)
+    .then(response => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const articles: { title: string; url: string | undefined; }[] = [];
+      $('article', html).each(function() {
+        const title = $(this).text();
+        const link = $(this).find('a').attr('href');
+        articles.push({ title, url: url + link });
+      });
+      console.log(articles);
+      res.status(200).send(articles);
+    })
+    .catch(error => {
+      console.error(error)
+      res.status(500).send(error);
+    });
+});
+
+app.use('/.netlify/functions/index', app);
+export const handler = ServerlessHttp(app);
+
+//app.listen(3001, () => console.log(`Server running on port 3001`));
